7cd7bec09371bac71c38d1397c0dc78333216a12
jprittie <eyewallop@gmail.com>
2017-02-10 21:41:23 -0500

    Shows JSX commenting style

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
 class StorePicker extends React.Component {
+  // This style of commenting can be used here
   render() {
-    return <p>Hello World!</p>
+    return (
+      <form className="store-selector">
+        { /* But here, JSX comment style is needed */ } 
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store</button>
+      </form>
+    )
   }
 }
 
