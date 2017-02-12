4e73ff6674cff5f37e13b1102ba93e5f01466370
jprittie <eyewallop@gmail.com>
2017-02-12 10:38:59 -0500

    Adds ref on input

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,13 +2,25 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+
+  goToStore(event) {
+    event.preventDefault();
+    console.log("You changed the URL")
+    // First, grab the text from the box
+    // Second, transition from "/" to "/store/:storeID"
+
+  }
+
+
+
   // This style of commenting can be used here
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         { /* But here, JSX comment style is needed */}
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+        { /* Ref - when the input is rendered, there will be a reference to the input on the class itself*/ }
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input }} />
         <button type="submit">Visit Store</button>
       </form>
     )
