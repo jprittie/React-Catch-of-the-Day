03d77cd485f3e3aec1f707c55f638e76472d0ec2
jprittie <eyewallop@gmail.com>
2017-02-12 09:47:22 -0500

    Imports helpers file

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
   // This style of commenting can be used here
@@ -7,7 +8,7 @@ class StorePicker extends React.Component {
       <form className="store-selector">
         { /* But here, JSX comment style is needed */}
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
         <button type="submit">Visit Store</button>
       </form>
     )
