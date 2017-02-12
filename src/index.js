36941bf8d44e0e662278a00ab635b99fe9d0953f
jprittie <eyewallop@gmail.com>
2017-02-11 20:12:19 -0500

    Adds routing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
+
 import './css/style.css';
-import App from './components/App'
+
+import App from './components/App';
 import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
 
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        { /*Match could have two tags or one self-closing one */}
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match pattern="/store/:storeID" component={App} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
 
-render(<App/>, document.querySelector('#main'));
+  )
+
+}
+
+render(<Root/>, document.querySelector('#main'));
